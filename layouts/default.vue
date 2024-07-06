85faa3a3462d4e67b1f6e94895492541c7fccab4
divyank22 <divyankpandey37@gmail.com>
2024-07-06 13:07:01 +0530

    start
    install tailwind setup layout index page

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+        <nav class="flex w-auto bg-blue-600 justify-between shadow-2xl">
+            <h1 class="text-3xl" >ecom</h1>
+            <ul class="flex list-none">
+                <li class="mx-1 md:mx-4 my-auto">home</li>
+                <li class="mx-1 md:mx-4 my-auto">product</li>
+                <li class="mx-1 md:mx-4 my-auto">about</li>
+            </ul>
+        </nav>
+    </header>
+    <div>
+        <slot></slot>
+    </div>
+    <footer></footer>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="container flex justify-center h-96 items-center">
+        <h1 class="text-center">welcome to ecom</h1>
+    </div>
+</template>
